fe9f6b595886c11c65c48aa6182a9d9198e140dc
fsgom <fernandosegom@gmail.com>
2017-07-14 17:42:42 -0500

    Clean backup of some code in this repository

--- dibujo/js/app.js
+++
@@ -1,7 +0,0 @@
-var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
-var Escenario=require('./escenario');
-var escenario=Escenario();
-escenario.init(SCREEN_WIDTH,SCREEN_HEIGHT);
-escenario.initWebcam(1000,700);
-escenario.initMarcador();
-escenario.render();
--- memoramafinal/js/app.js
+++
@@ -1,5 +0,0 @@
-//DEBUG=true;
-Memorama=require("../src/memorama.js");
-memorama=new Memorama();
-memorama.config({tipo_memorama:"cocina",cantidad_cartas:6});
-memorama.init();
